13defc3dc3c550b009a3c87ffa9cda7def127f79
zeljko <zeljko@z-ware.fi>
2015-10-31 12:46:52 +0200

    listen to all interfaces, port 3000
    In development we want to be able to access from other computers. Unfortunatelly,
    this is now blocked and Puma normally listens to localhost (127.0.0.1), this
    fix makes it listen on all ports. Notice that this might be security issue if
    you do not have firewall in place.
    
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,17 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+# Everything under this line has a goal of setting the Rack server (likely
+# Puma) to listen to all interfaces, not only localhost.
+require 'rails/commands/server'
+if Rails.env == 'development'
+  module Rails
+    class Server
+      alias_method :default_options_alias, :default_options
+      def default_options
+        default_options_alias.merge! Host: '0.0.0.0'
+      end
+    end
+  end
+end
