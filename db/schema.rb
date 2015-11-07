78fb725ea2cb0d1582b7bb18dd10d1fa6e679829
zeljko <zeljko@z-ware.fi>
2015-11-07 20:02:50 +0200

    users and organizations, membershi types
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106165848) do
+ActiveRecord::Schema.define(version: 20151106180645) do
+
+  create_table "memberships", force: :cascade do |t|
+    t.string   "membership_type", default: "member"
+    t.integer  "user_id"
+    t.integer  "organization_id"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+  end
+
+  add_index "memberships", ["organization_id"], name: "index_memberships_on_organization_id"
+  add_index "memberships", ["user_id"], name: "index_memberships_on_user_id"
 
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer  "resource_owner_id", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,53 @@
-User.delete_all
 Doorkeeper::Application.delete_all
 Doorkeeper::AccessGrant.delete_all
 Doorkeeper::AccessToken.delete_all
+Membership.delete_all
+User.delete_all
+Organization.delete_all
 
-User.create! email: 'rocky@zwr.fi', password: 'balboa123'
-User.create! email: 'z@zwr.fi', password: 'balboa123', admin: true
+rocky = User.create! email: 'rocky@zwr.fi', password: 'balboa123'
+z = User.create! email: 'z@zwr.fi', password: 'balboa123', admin: true
+
+File.open(Rails.root.join 'db', 'seeds', 'users.csv').each_with_index do |line, count|
+  u = line.split ','
+  User.create! email: u[6], password: u[8]
+  # name: "#{u[1]} #{u[2]}",
+  # gender: u[0],
+  # address_street: u[3],
+  # address_city: u[4].capitalize,
+  # address_post_number: u[5],
+  # email: u[6],
+  # username: u[7],
+  # password: u[8],
+  # date_of_birth: Date.strptime(u[9], '%m/%d/%Y'),
+  # cc_type: u[10],
+  # cc_number: u[11],
+  # cc_cvc: u[12],
+  # cc_exp: u[13],
+  # telnum: u[14]
+
+  break if count > 10
+end
+puts "Seeded #{User.count} users."
 
 Doorkeeper::Application.create! name: 'rpm',
                                 uid: '32258b408332a9504bf3d048a5879ed3b9978474d408b2f3c9bab1cb8a03cda3',
                                 secret: '96a730fb7a806fc1f6cb8e52b7d1f40b5314126978dad977bfcb5682c922760c',
                                 redirect_uri: 'http://192.168.6.108:3001/auth/'
+
+puts "Seeded #{Doorkeeper::Application.count} apps."
+
+Organization.create! name: 'Balboa', # this is the admin Application
+                     memberships: [Membership.new(user: z, membership_type: 'admin')]
+
+m = Organization.new name: 'Mohican'
+m.memberships.new user: rocky, membership_type: 'admin'
+m.users << User.all.sample(5)
+m.save!
+
+m = Organization.new name: 'Celesta'
+m.users << User.all.sample(3)
+m.memberships.first.membership_type = 'admin'
+m.save!
+
+puts "Seeded #{Organization.count} organizations."
